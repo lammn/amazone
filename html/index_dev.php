768f98d651f3852e7e429d89c6aa020cf7459ed4
lammn90 <lammn90@gmail.com>
2017-05-27 04:48:50 +0000

    fix static url

--- html/index_dev.php
+++ html/index_dev.php
@@ -28,19 +28,6 @@ use Symfony\Component\Yaml\Yaml;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
-$allow = array(
-    '127.0.0.1',
-    'fe80::1',
-    '::1',
-);
-
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], $allow)
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
 
 //[INFO]index.php,install.phpをEC-CUBEルート直下に移動させる場合は、コメントアウトしている行に置き換える
 require_once __DIR__.'/../autoload.php';
